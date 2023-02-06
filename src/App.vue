531272f9507695b8379b2fe95936861be159198b
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-02-06 15:28:50 +0100

    inizializzazione progetto  e scaffolding di base

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    AppHeader,
+  }
+}
+
+</script>
+
+<template>
+  <AppHeader/>
+  
+</template>
+
+<style lang="scss">
+@import './styles/main.scss'
+
+</style>
